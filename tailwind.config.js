f7220773b520871b48bd1944e02040a93f9e87f1
Prathamesh-Kapade <prathameshkapade6+github@gmail.com>
2025-09-15 10:09:07 +0530

    Updated feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 // tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class", 
     content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+
   theme: {
     extend: {},
+    fontFamily: {
+      display:['Synce']
   },
   plugins: [], 
 }
+};
